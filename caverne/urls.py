8341d6b87a409c9f16016c42baa916671b382e6b
oliviernes <olivier.nesme@gmail.com>
2021-11-05 20:58:49 +0100

    Include joueur and accounts in caverne's url patterns.

--- caverne/urls.py
+++ caverne/urls.py
@@ -17,5 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path("", include("joueur.urls")),
+    path("", include("accounts.urls")),
     path('admin/', admin.site.urls),
 ]
