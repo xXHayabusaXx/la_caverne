aed9799f9189aa4eaf162de66cb80846abb32e7e
oliviernes <olivier.nesme@gmail.com>
2021-11-05 20:52:32 +0100

    Add custom authentication backend

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+"""Models for accounts package"""
 
-# Create your models here.
+from django.contrib.auth import get_user_model
+
+
+class EmailBackend(object):
+    """custom authentication backend"""
+
+    def authenticate(self, request, username=None, password=None):
+        """Method to authenticate"""
+        user_model = get_user_model()
+        try:
+            user = user_model.objects.get(email=username)
+        except user_model.DoesNotExist:
+            return None
+        else:
+            if getattr(user, "is_active", False) and user.check_password(
+                password
+            ):
+                return user
+        return None
+
+    def get_user(self, user_id):
+        user_model = get_user_model()
+        try:
+            return user_model.objects.get(pk=user_id)
+        except user_model.DoesNotExist:
+            return None
