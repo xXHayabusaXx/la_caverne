aed81c080c63ceeccd328470d90888842668aa74
oliviernes <olivier.nesme@gmail.com>
2021-11-06 19:35:23 +0100

    Add view calcul in joueur/views.py

--- joueur/views.py
+++ joueur/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def calcul(request):
+    """Display calcul results and save user's personnage information."""
+
+    return render(request, "joueur/perso.html", context)
