e15132061e88f2951922c93d757c8de98bce2071
oliviernes <olivier.nesme@gmail.com>
2021-11-06 16:53:36 +0100

    Add AUTHENTICATION_BACKENDS in settings.py

--- caverne/settings.py
+++ caverne/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
