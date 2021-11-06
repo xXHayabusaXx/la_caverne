6f37f7f9b5c1814599614841deddc84b4cc732ee
oliviernes <olivier.nesme@gmail.com>
2021-11-06 16:57:26 +0100

    Import include in caverne/urls.py

--- caverne/settings.py
+++ caverne/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'joueur.apps.JoueurConfig',
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
