f9156545a569bd14659c098a32f3d159472c5a42
oliviernes <olivier.nesme@gmail.com>
2021-11-06 16:14:48 +0100

    Add accounts in INSTALLED_APPS

--- caverne/settings.py
+++ caverne/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'joueur.apps.JoueurConfig',
+    'accounts.apps.AccountsConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #local
-    'joueur.apps.JoueurConfig',
 ]
 
 MIDDLEWARE = [
