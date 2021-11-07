efbd2cb7ef4df384351f4081f60898ccf123e604
oliviernes <olivier.nesme@gmail.com>
2021-11-07 21:30:21 +0100

    Add form_perso to perso.html from CreaPersoForm()

--- joueur/views.py
+++ joueur/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .forms import CreaPersoForm
+
 def calcul(request):
     """Display calcul results and save user's personnage information."""
 
-    return render(request, "joueur/perso.html")
+    if request.method == "POST":
+        form_perso = CreaPersoForm(request.POST)
+
+    form_perso = CreaPersoForm()
+
+    return render(request, "joueur/perso.html", { "form_perso": form_perso })
