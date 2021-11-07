64ea67300a4df4b9d99ea9113a1c7bf9cdc00686
oliviernes <olivier.nesme@gmail.com>
2021-11-07 19:11:29 +0100

    add tests login_wrong_password and no_user_recorded

--- test/test_views.py
+++ test/test_views.py
@@ -49,3 +49,33 @@ class TestLogin:
         assert response.status_code == 200
         assert response.templates[0].name == "registration/login.html"
         assert response.templates[1].name == "joueur/base.html"
+
+    @mark.django_db
+    def test_login_wrong_password(self):
+
+        User.objects.create_user(
+            "john", "lennon@thebeatles.com", "johnpassword"
+        )
+
+        response_login = self.client.login(
+            username="lennon@thebeatles.com", password="wrongpassword"
+        )
+
+        response_post = self.client.post(
+            "/login/",
+            {"username": "lennon@thebeatles.com", "password": "wrongpassword"},
+        )
+
+        assert response_login == False
+        assert response_post.status_code == 200
+        assert response_post.templates[0].name == "registration/login.html"
+        assert response_post.templates[1].name == "joueur/base.html"
+
+    @mark.django_db
+    def test_login_no_user_recorded(self):
+
+        response = self.client.login(
+            username="lennon@thebeatles.com", password="johnpassword"
+        )
+
+        assert response == False
