8a773222f2330bf37bed514fb1c758682bed9c03
oliviernes <olivier.nesme@gmail.com>
2021-11-07 18:52:19 +0100

    Add test login_valid_user and login_wrong_user

--- test/test_views.py
+++ test/test_views.py
@@ -1,3 +1,5 @@
+from pytest import mark
+
 from django.test import Client
 from django.contrib.auth.models import User
 
@@ -18,3 +20,32 @@ class TestLogin:
         assert response.templates[0].name == "registration/login.html"
         assert response.templates[1].name == "joueur/base.html"
 
+    @mark.django_db
+    def test_login_valid_user(self):
+        User.objects.create_user(
+            "john", "lennon@thebeatles.com", "johnpassword"
+        )
+
+        response_login = self.client.login(
+            username="lennon@thebeatles.com", password="johnpassword"
+        )
+
+        response_post = self.client.post(
+            "/login/",
+            {"username": "lennon@thebeatles.com", "password": "johnpassword"},
+        )
+
+        assert response_login == True
+        assert response_post.url == "/my_account/"
+        assert response_post.status_code == 302
+
+    @mark.django_db
+    def test_login_wrong_user(self):
+
+        response = self.client.post(
+            "/login/", {"username": "tartampion", "password": "johnpassword"}
+        )
+
+        assert response.status_code == 200
+        assert response.templates[0].name == "registration/login.html"
+        assert response.templates[1].name == "joueur/base.html"
