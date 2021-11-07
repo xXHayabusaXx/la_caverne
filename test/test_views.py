c153c80ada60aaa6ad16e4eb88fa1fb231c92114
oliviernes <olivier.nesme@gmail.com>
2021-11-07 20:09:28 +0100

    Add signup tests

--- test/test_views.py
+++ test/test_views.py
@@ -79,3 +79,94 @@ class TestLogin:
         )
 
         assert response == False
+
+
+####################
+#   signup view    #
+####################
+
+
+class TestSignup:
+
+    client = Client()
+
+    def test_signup(self):
+
+        response = self.client.get("/signup/")
+
+        assert response.status_code == 200
+        assert response.templates[0].name == "registration/signup.html"
+        assert response.templates[1].name == "joueur/base.html"
+
+    @mark.django_db
+    def test_signup_right_infos(self):
+
+        response = self.client.post(
+            "/signup/",
+            {
+                "username": "Mell1",
+                "first_name": "Mell",
+                "last_name": "MAMAMA",
+                "email": "mell6@gmail.com",
+                "password1": "monsupermdp1234",
+                "password2": "monsupermdp1234",
+            },
+        )
+
+        users = User.objects.all()
+
+        assert response.url == "/my_account/"
+        assert response.status_code == 302
+        assert users.count() == 1
+        assert users[0].username == "Mell1"
+        assert users[0].first_name == "Mell"
+        assert users[0].last_name == "MAMAMA"
+        assert users[0].email == "mell6@gmail.com"
+
+    @mark.django_db
+    def test_signup_user_incorrect_data(self):
+
+        response = self.client.post(
+            "/signup/",
+            {
+                "username": "",
+                "first_name": "",
+                "last_name": "",
+                "email": "pimail.com",
+                "password1": "aa",
+                "password2": "bb",
+            },
+        )
+
+        users = User.objects.all()
+
+        assert response.status_code == 200
+        assert response.templates[0].name == "registration/signup.html"
+        assert response.templates[1].name == "joueur/base.html"
+        assert users.count() == 0
+
+    @mark.django_db
+    def test_signup_user_email_already_used(self):
+
+        User.objects.create_user(
+            "john", "lennon@thebeatles.com", "johnpassword"
+        )
+
+        response = self.client.post(
+            "/signup/",
+            {
+                "username": "Mell1",
+                "first_name": "Mell",
+                "last_name": "MAMAMA",
+                "email": "lennon@thebeatles.com",
+                "password1": "monsupermdp1234",
+                "password2": "monsupermdp1234",
+            },
+        )
+
+        users = User.objects.all()
+
+        assert response.status_code == 200
+        assert response.templates[0].name == "registration/signup.html"
+        assert response.templates[1].name == "joueur/base.html"
+        assert users.count() == 1
