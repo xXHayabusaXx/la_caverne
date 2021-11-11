72a3cf926425e44503ca869a32f83ab6bf643ea6
oliviernes <olivier.nesme@gmail.com>
2021-11-11 21:58:32 +0100

    Add classe and race forms.ModelMultipleChoiceField with Select widgets

--- joueur/forms.py
+++ joueur/forms.py
@@ -7,6 +7,18 @@ from .models import Classe, Race, Personnages, Def, Carac
 class CreaPersoForm(ModelForm):
     "Create personnage form"
 
+
+    classe = forms.ModelMultipleChoiceField(
+        queryset=Classe.objects.all().order_by("nom"),
+        widget=forms.Select,
+    )
+
+    race = forms.ModelMultipleChoiceField(
+        queryset=Race.objects.all().order_by("nom"),
+        widget=forms.Select,
+    )
+
+
     class Meta:
         model = Personnages
         fields = [
@@ -19,6 +31,7 @@ class CreaPersoForm(ModelForm):
             "divinite",
             "initiative",
             "point_carac",
+            "classe",
+            "race"
         ]
-
 
